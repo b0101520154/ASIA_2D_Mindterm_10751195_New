0a11a72049c26cd3605ef37f6d3f1ab8d57fba45
b0101520154 <71687764+b0101520154@users.noreply.github.com>
2020-11-19 22:33:21 +0800

    上傳

---
+++ 2D_Midterm_107051195/Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public GameObject final;
+    public Text textCount;
+    public int count;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.name == "傳送門")
+        {
+            final.SetActive(true);
+        }
+        if (collision.tag == "星星")
+        {
+           Destroy(collision.gameObject);
+           count++;
+           textCount.text = count + "/3";
+
+        }
+    }
+}
